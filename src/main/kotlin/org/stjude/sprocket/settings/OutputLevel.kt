38bdbf9c537a637c71e3ec69180a88694ccd97a5
Serial <69764315+Serial-ATA@users.noreply.github.com>
2026-03-06 12:55:55 -0500

    feat: add more output levels

--- src/main/kotlin/org/stjude/sprocket/settings/OutputLevel.kt
+++ src/main/kotlin/org/stjude/sprocket/settings/OutputLevel.kt
@@ -1,6 +1,8 @@
 package org.stjude.sprocket.settings
 
 enum class OutputLevel(val displayName: String, val cliArg: String?) {
+    TRACE("Trace", "-vvv"),
+    DEBUG("Debug", "-vv"),
     VERBOSE("Verbose", "--verbose"),
     INFORMATION("Information", null),
     QUIET("Quiet", "--quiet");
